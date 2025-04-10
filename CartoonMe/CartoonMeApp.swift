89f3e4ff2b63195befb1d93d96e780896e1aa603
Yuri Petrosyan <yuriyuri200117@gmail.com>
2025-04-10 11:30:12 +0200

    Initial Commit

---
+++ CartoonMe/CartoonMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartoonMeApp.swift
+//  CartoonMe
+//
+//  Created by Yuri Petrosyan on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CartoonMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CartoonMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CartoonMe
+//
+//  Created by Yuri Petrosyan on 10/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
