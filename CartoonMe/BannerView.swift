48ad8912fad1f506ad9f019aa27fdb6adfe88318
Yuri Petrosyan <yuriyuri200117@gmail.com>
2025-04-11 11:35:21 +0200

    BannerView

---
+++ CartoonMe/BannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BannerView.swift
+//  CartoonMe
+//
+//  Created by Yuri Petrosyan on 11/04/2025.
+//
+
+import SwiftUI
+
+struct BannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerView()
+}
